5c87270707a6afbbc89cfaa36ae50946538692d1
e-chernov <evgeniy.chernov@gmail.com>
2017-11-15 16:06:02 +0300

    before store building

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
-import 'babel-polyfill'
 import React from 'react'
 import { render } from 'react-dom'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
 import App from './containers/App'
 
+const store = configureStore()
 
 render(
-<App />,
+    <Provider store={store}>
+        <App />
+    </Provider>,
     document.getElementById('root')
 )
