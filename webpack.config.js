8292e35f1fdcdd12ff82159729dd24c258e21f1f
e-chernov <evgeniy.chernov@gmail.com>
2017-11-15 14:19:59 +0300

    working

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,7 @@ module.exports = {
     devtool: 'cheap-module-eval-source-map',
     entry: [
         'webpack-hot-middleware/client',
+        'babel-polyfill',
         './src/index'
     ],
     output: {
@@ -13,8 +14,19 @@ module.exports = {
         publicPath: '/static/'
     },
     plugins: [
-        new webpack.optimize.OccurenceOrderPlugin(),
-        new webpack.HotModuleReplacementPlugin(),
-        new webpack.NoErrorsPlugin()
-    ]
+        new webpack.optimize.OccurrenceOrderPlugin(),
+        new webpack.HotModuleReplacementPlugin()
+    ],
+    module: { //Обновлено
+        loaders: [ //добавили babel-loader
+            {
+                loaders: ['babel-loader'],
+                include: [
+                    path.resolve(__dirname, "src"),
+                ],
+                test: /\.js$/,
+                plugins: ['transform-runtime'],
+            }
+        ]
+    }
 }
